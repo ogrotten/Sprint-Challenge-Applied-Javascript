05bd198f854f9584cf514a9684eb1bfd80416046
Darrin Lowery <lambda@monsto.com>
2019-10-04 11:53:30 -0500

    cards display complete

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -19,28 +19,43 @@
 // Create a card for each of the articles and add the card to the DOM.
 
 function createCard(data) {
+
 	const outer = DCE("div");
     outer.classList = "card";
     
 	const headline = DCE("div");
-	headline.classList = "headline";
-    headline.textContent = "headline" //`${headline}`
+        headline.classList = "headline";
+        headline.textContent = data.headline
     
     const author = DCE("div");
-    author.classList = "author";
-    const avcontainer = DCE("div")
-    avcontainer.classList = "img-container";
-    const avatar = DCE("img")
-    avatar.src = "url"
-    const byline = DCE("span")
-    byline.textContent = "author"
+        author.classList = "author";
+        const avcontainer = DCE("div");
+            avcontainer.classList = "img-container";
+            const avatar = DCE("img");
+            avatar.src = data.authorPhoto;
+        const byline = DCE("span");
+        byline.textContent = data.authorName;
 
-    avcontainer.append(avatar)
-    author.append(avcontainer, byline)
+    avcontainer.append(avatar);
+    author.append(avcontainer, byline);
     
-    outer.append(headline, author)
-    return outer;
+    outer.append(headline, author);
+    document.querySelector(".cards-container").append(outer);
 }
 
-clg(createCard())
-// document.querySelector(".cards-container").append(createCard())
+// clg(createCard())
+
+
+axios
+	.get("https://lambda-times-backend.herokuapp.com/articles")
+	.then(res => {
+        data = res.data;
+        Object.values(data.articles).forEach(e => {
+            e.forEach(f => {
+                createCard(f);
+            });
+        });
+	})
+	.catch(err => {
+		clg(`problem with cards: ${err}`);
+	});
